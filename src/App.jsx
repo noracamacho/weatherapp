682816a152d6bfbd6288422bdc2ed28c090df222
noracamacho <ingnorac@gmail.com>
2022-12-30 14:12:12 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import img04d from './images/04d.jpg'
+import backgroundImages from './helpers/backgroundImages'
+import './App.css'
+
+
+
+function App() {
+  const [data, setData] = useState({});
+  const [units, setUnits] = useState('metric'); 
+  const [icon, setIcon] = useState('');
+  const [temp, setTemp] = useState(0);
+  const [img, setImg] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+        let lat = pos.coords.latitude;
+        let lon = pos.coords.longitude;
+        
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0c5cfcfe9fc145d412e30508b4a89137&units=${units}`)
+      .then(response => setData(response.data))
+      .then(console.log(data))
+      .catch(error => console.log('error', error));
+    });
+
+  }, []);
+
+  useEffect(() => {
+    if(data) {
+      console.log(data);
+      setIcon(data?.weather?.[0].icon);
+      setTemp(data.main?.temp.toFixed());
+      setImg(data?.weather?.[0].main + data?.weather?.[0].icon);
+      console.log(img);
+    }
+  }, [data]);
+
+   // metric
+ function changeUnits() {
+  let currentTemp;
+    if(units === 'metric') {
+      currentTemp = (temp * 9 / 5) +32;
+      setTemp(currentTemp.toFixed());
+      console.log('f',temp);
+      setUnits('imperial');
+    } else {
+      currentTemp = (temp - 32) * (5/9);
+      setTemp(currentTemp.toFixed());
+      console.log('c', temp);
+      setUnits('metric');
+    }
+  }
+
+  let date = new Date();
+  console.log(date.toISOString().split('T')[0])
+  
+  return (
+    <div className='app' style={{  
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.45), 
+      rgba(0, 0, 0, 0.45)), url(${img04d})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    }}>
+      <div className="container">
+        <div className="top">
+          <div className="buttons__container">
+            <button onClick={changeUnits}>{units === 'imperial' ? '°C' : '°F'}</button>
+         </div>
+          <div className="location">
+            <p>{`${data?.name}, ${data.sys?.country}`}</p>
+          </div>
+          <div className="temp">
+            <h1>{temp}{units === 'imperial' ? '°F' : '°C'}</h1>
+            {
+              icon && (<img className='iconImage' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />)
+            }
+          </div>
+            <div className="despcription">
+              <p>{data?.weather?.[0].description}</p>
+            </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>{data.clouds?.all}%</p>
+            <p>Clouds</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>{data?.main?.humidity}%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className='bold'>{data?.wind?.speed.toFixed()}{units === 'imperial' ? 'm/h' : 'm/s'}</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/helpers/backgroundImages.js
@@ -0,0 +1,50 @@
+import clearsky from '../images/01d.jpg';
+import fewclouds from '../images/02d.jpg';
+import scatteredclouds from '../images/03d.jpg';
+import brokenclouds from '../images/04d.jpg';
+import showerrain from '../images/09d.jpg';
+import rain from '../images/10d.jpg';
+import thunderstorm from '../images/11d.jpg';
+import snow from '../images/13d.jpg';
+import mist from '../images/50d.jpg';
+import Clear01n from '../images/Clear01n.jpg';
+
+const backgroundImages = {   
+        '01d': {
+            image: clearsky
+        },
+        '02d': {
+            image: fewclouds
+        },
+ 
+        '03d': {
+            image: scatteredclouds
+        },
+
+        '04d': {
+            image: brokenclouds
+        },
+  
+        '09d': {
+            image: showerrain
+        },
+        '10d': {
+            image: rain
+        },
+        '11d': {
+            image: thunderstorm
+        },
+        '13d': {
+            image: snow
+        },
+        '50d': {
+            image: mist
+        },
+        'Clear01n': {
+            image: Clear01n
+        }
+}
+
+export default backgroundImages
+
+

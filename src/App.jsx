812b42d85eb4b203ca60f56edae6f2505fd89dac
noracamacho <ingnorac@gmail.com>
2023-01-02 22:15:08 -0600

    addign weather icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios' 
 import backgroundImages from './data/backgroundImages'
+import countryCode from './data/countryCode'
+import 'weather-icons/css/weather-icons.css';
 import './App.css'
 
 const idKey = '0c5cfcfe9fc145d412e30508b4a89137';
@@ -31,24 +33,25 @@ function App() {
       setIcon(data?.weather?.[0].icon);
       setTemp(data.main?.temp.toFixed());
       setImg(data?.weather?.[0].main + data?.weather?.[0].icon);
-      console.log(img);
+      // console.log(img);
     }
   }, [data]);
 
-  console.log(backgroundImages[icon])
+  // console.log(backgroundImages[icon])
+  // console.log(countryCode[data.sys?.country]);
 
-   // metric
+   // °C to °F
  function changeUnits() {
   let currentTemp;
     if(units === 'metric') {
       currentTemp = (temp * 9 / 5) +32;
       setTemp(currentTemp.toFixed());
-      console.log('f',temp);
+      // console.log('f',temp);
       setUnits('imperial');
     } else {
       currentTemp = (temp - 32) * (5/9);
       setTemp(currentTemp.toFixed());
-      console.log('c', temp);
+      // console.log('c', temp);
       setUnits('metric');
     }
   }
@@ -58,8 +61,8 @@ function App() {
   
   return (
     <div className='app' style={{  
-      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.45), 
-      rgba(0, 0, 0, 0.45)), url(${backgroundImages[icon]})`,
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.35), 
+      rgba(0, 0, 0, 0.35)), url(${backgroundImages[icon]})`,
       backgroundPosition: 'center',
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat'
@@ -70,7 +73,7 @@ function App() {
             <button onClick={changeUnits}>{units === 'imperial' ? '°C' : '°F'}</button>
          </div>
           <div className="location">
-            <p>{`${data?.name}, ${data.sys?.country}`}</p>
+            <p>{`${data?.name}, ${countryCode[data.sys?.country]}`}</p>
           </div>
           <div className="temp">
             <h1>{temp}{units === 'imperial' ? '°F' : '°C'}</h1>
@@ -84,15 +87,18 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
+            <i className='wi wi-cloud'></i>
             <p className='bold'>{data.clouds?.all}%</p>
             <p>Clouds</p>
           </div>
           <div className="humidity">
+            <i className='wi wi-sprinkle'></i>
             <p className='bold'>{data?.main?.humidity}%</p>
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className='bold'>{data?.wind?.speed.toFixed()}{units === 'imperial' ? 'm/h' : 'm/s'}</p>
+            <i className='wi wi-strong-wind'></i>
+            <p className='bold'>{data?.wind?.speed.toFixed()}m/s</p>
             <p>Wind Speed</p>
           </div>
         </div>

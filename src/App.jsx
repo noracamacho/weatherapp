37efed433458a7cca9ef5a5d770f9cfd055a9e79
noracamacho <ingnorac@gmail.com>
2023-01-03 01:22:07 -0600

    Adding Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,147 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios' 
-import backgroundImages from './data/backgroundImages'
-import countryCode from './data/countryCode'
-import 'weather-icons/css/weather-icons.css';
-import sunset from './images/sunset.jpg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import backgroundImages from "./data/backgroundImages";
+import countryCode from "./data/countryCode";
+import "weather-icons/css/weather-icons.css";
+import ClipLoader from "react-spinners/ClipLoader";
+import { Spinner } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-const idKey = '0c5cfcfe9fc145d412e30508b4a89137';
+const idKey = "0c5cfcfe9fc145d412e30508b4a89137";
 
 function App() {
   const [data, setData] = useState({});
-  const [units, setUnits] = useState('metric'); 
+  const [units, setUnits] = useState("metric");
   const [icon, setIcon] = useState();
   const [temp, setTemp] = useState(0);
   const [tempFeelsLike, setTempFeelsLike] = useState(0);
-  const [img, setImg] = useState(sunset);
+  // const [loading, setLoading] = useState(true);
+  // const [img, setImg] = useState(sunset);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(pos => {
-        let lat = pos.coords.latitude;
-        let lon = pos.coords.longitude;
-        
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${idKey}&units=${units}`)
-      .then(response => setData(response.data))
-      .then(console.log(data))
-      .catch(error => console.log('error', error));
-    });
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let lat = pos.coords.latitude;
+      let lon = pos.coords.longitude;
 
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${idKey}&units=${units}`
+        )
+        .then((response) => setData(response.data))
+        .then(console.log(data))
+        .catch((error) => console.log("error", error));
+    });
   }, []);
 
   useEffect(() => {
-    if(data) {
+    if (data) {
       console.log(data);
       setIcon(data?.weather?.[0].icon);
       setTemp(data.main?.temp.toFixed(0));
-      setTempFeelsLike(data?.main?.feels_like.toFixed(0))
+      setTempFeelsLike(data?.main?.feels_like.toFixed(0));
+      // setLoading(false);
     }
   }, [data]);
 
-   // °C to °F
- function changeUnits() {
-  let currentTemp;
-  let feelsLikeTemp;
-    if(units === 'metric') {
-      currentTemp = (temp * 9 / 5) +32;
-      feelsLikeTemp = (tempFeelsLike * 9 / 5) +32;
+  // °C to °F
+  function changeUnits() {
+    let currentTemp;
+    let feelsLikeTemp;
+    if (units === "metric") {
+      currentTemp = (temp * 9) / 5 + 32;
+      feelsLikeTemp = (tempFeelsLike * 9) / 5 + 32;
       setTemp(currentTemp.toFixed());
       setTempFeelsLike(feelsLikeTemp.toFixed());
-      setUnits('imperial');
+      setUnits("imperial");
     } else {
-      currentTemp = (temp - 32) * (5/9);
-      feelsLikeTemp = (tempFeelsLike - 32) * (5/9);
+      currentTemp = (temp - 32) * (5 / 9);
+      feelsLikeTemp = (tempFeelsLike - 32) * (5 / 9);
       setTemp(currentTemp.toFixed());
       setTempFeelsLike(feelsLikeTemp.toFixed());
-      setUnits('metric');
+      setUnits("metric");
     }
   }
-  // rgba(0, 0, 0, 0.45)), url(${backgroundImages[icon]})`,
-  // let date = new Date();
-  // console.log(date.toISOString().split('T')[0])  
   return (
-    <div className='app' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${backgroundImages[icon]})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-      <div className="container">
-        <div className="top">
-          <div className="buttons__container">
-            <button onClick={changeUnits}>{units === 'imperial' ? '°C' : '°F'}</button>
-         </div>
-          <div className="location">
-            {icon && (<p>{`${data?.name}, ${countryCode[data.sys?.country]}`}</p>)}
-          </div>
-          <div className="temp">
-            { icon && ( <h1>{temp}{units === 'imperial' ? '°F' : '°C'}</h1>) }
-            { icon && (<img className='iconImage' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather icon" />) }
-          </div>
-            <div className="despcription">
-              <p>{data?.weather?.[0].description}</p>
+    <>
+      {icon === undefined ? (
+        <div className="app__spinner" style={{ backgroundColor: "#eff4f8", alignContent: 'center' }}>
+          <Spinner color="dark" size='large' />
+        </div>
+      ) : (
+        <div
+          className="app"
+          style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${backgroundImages[icon]})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="container">
+            <div className="top">
+              <div className="buttons__container">
+                <button onClick={changeUnits}>
+                  {units === "imperial" ? "°C" : "°F"}
+                </button>
+              </div>
+              <div className="location">
+                {icon && (
+                  <p>{`${data?.name}, ${countryCode[data.sys?.country]}`}</p>
+                )}
+              </div>
+              <div className="temp">
+                {icon && (
+                  <h1>
+                    {temp}
+                    {units === "imperial" ? "°F" : "°C"}
+                  </h1>
+                )}
+                {icon && (
+                  <img
+                    className="iconImage"
+                    src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+                    alt="weather icon"
+                  />
+                )}
+              </div>
+              <div className="despcription">
+                <p>{data?.weather?.[0].description}</p>
+              </div>
             </div>
+            <div className="bottom">
+              <div className="feels">
+                <i className="wi wi-thermometer-exterior"></i>
+                <p className="bold">
+                  {tempFeelsLike}
+                  {units === "imperial" ? "°F" : "°C"}
+                </p>
+                <p>Feels</p>
+              </div>
+              <div className="clouds">
+                <i className="wi wi-cloud"></i>
+                <p className="bold">{data.clouds?.all}%</p>
+                <p>Clouds</p>
+              </div>
+              <div className="humidity">
+                <i className="wi wi-sprinkle"></i>
+                <p className="bold">{data?.main?.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                <i
+                  className="wi wi-strong-wind"
+                  style={{ fontWeight: "bold" }}
+                ></i>
+                <p className="bold">{data?.wind?.speed.toFixed()}m/s</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="bottom">
-        <div className="feels">
-            <i className='wi wi-thermometer-exterior'></i>
-            <p className='bold'>{tempFeelsLike}{units === 'imperial' ? '°F' : '°C'}</p>
-            <p>Feels</p>
-          </div>
-          <div className="clouds">
-            <i className='wi wi-cloud'></i>
-            <p className='bold'>{data.clouds?.all}%</p>
-            <p>Clouds</p>
-          </div>
-          <div className="humidity">
-            <i className='wi wi-sprinkle'></i>
-            <p className='bold'>{data?.main?.humidity}%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <i className='wi wi-strong-wind' style={{fontWeight: 'bold'}}></i>
-            <p className='bold'>{data?.wind?.speed.toFixed()}m/s</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
